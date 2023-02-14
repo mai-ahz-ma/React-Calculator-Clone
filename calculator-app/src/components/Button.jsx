bfc20da608398a36236f21a7e7bc2eb6b4037745
mai <cmanzines15@gmail.com>
2023-02-14 01:56:02 -0500

    calculator

---
+++ calculator-app/src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Button.css";
+
+const isOperator = val => {
+
+    return !isNaN(val) || val === "." || val ==="=";
+
+}
+
+export const Button = props => ( 
+    <div 
+        className={`button-wrapper ${
+            isOperator(props.children) ? null : "operator"
+       }`}
+
+        >
+            {props.children}
+    </div>
+);
+
+
+export default Button;
---
+++ calculator-app/src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Input.css'
+
+
+export const Input = (props) => (
+
+    <div className='Input'>
+      {props.Input}
+    </div>
+)
+
+
+
+export default Input
